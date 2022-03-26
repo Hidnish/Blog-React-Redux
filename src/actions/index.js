82751b1cf29bdb2e81441d5b50234229bcd804c8
Hidnish <hideonishimura@live.it>
2022-03-26 16:41:13 +0100

    Final commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,41 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-// function that returns a function
-export const fetchPosts = () => async (dispatch) => {
-	const response = await jsonPlaceholder.get("/posts");
 
-	// call dispatch only when response arrives
-	dispatch({ type: "FETCH_POSTS", payload: response.data });
-};
+// functions that returns FUNCTIONS
 
-export const fetchUser = (userId) => dispatch => {
-    _fetchUser(userId, dispatch);
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id))); 
+
+    _.chain(getState().posts)
+      .map('userId')
+      .uniq()
+      .forEach(id => dispatch(fetchUser(id))) // 'await' does not work inside forEach
+      .value();
 }
 
-// declare the memoize (_fetchUser) outside so that it is not called everytime fetchUser is called
-const _fetchUser = _.memoize( async (id, dispatch) => {
+export const fetchPosts = () => async dispatch => {
+	const response = await jsonPlaceholder.get("/posts");
+
+	dispatch({ type: "FETCH_POSTS", payload: response.data }); // call dispatch only when response arrives
+};
+
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    // when you fetch one single item (i.e. 1 user) it comes back as an object (not array)
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
 
+
+
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+
+// declare the memoize (_fetchUser) outside so that it is not called everytime fetchUser is called
+// const _fetchUser = _.memoize( async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
